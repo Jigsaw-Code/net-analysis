2a4e0db01f299f74cea0a6079e2c83c47cbd75eb
Vinicius Fortuna <fortuna@google.com>
2018-03-04 21:38:40 -0500

    Create iterator over all report files.

--- netanalysis/ooni/list_ooni_files.py
+++ netanalysis/ooni/list_ooni_files.py
@@ -16,41 +16,58 @@ import argparse
 import itertools
 import logging
 import os.path
+import pathlib
 import sys
 import tarfile
+from typing import Iterable, TextIO, Tuple
 
 import boto3
 from botocore.handlers import disable_signing
-from botocore.response import StreamingBody
+import botocore.response
 import lz4.frame
 
-def list_files(bucket, test_name="web_connectivity", prefix=""):
-    for obj in bucket.objects.filter(Prefix="autoclaved/jsonl.tar.lz4/" + prefix):
-        filename = obj.key.rsplit("/", 1)[-1]
-        if not test_name or test_name in filename:
-            yield obj
-   
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(name="list_ooni_files")  # type: logging.Logger
+
+def read_report_files(ooni_bucket, test_name="web_connectivity", prefix="") -> Iterable[Tuple[str, TextIO]]:
+    """
+    Returns a pair (<filename>, <fileobj>) for each OONI report file.
+
+    This generator takes care of navigating the hybrid structure in the OONI dataset.
+    """
+    REPORTS_ROOT = pathlib.PurePosixPath("autoclaved/jsonl.tar.lz4")
+    for s3_obj in ooni_bucket.objects.filter(Prefix=str(REPORTS_ROOT / prefix)):
+        report_path = pathlib.PurePosixPath(s3_obj.key).relative_to(REPORTS_ROOT)
+        LOGGER.debug("Found S3 file %s", s3_obj.key)
+        if test_name and (test_name not in report_path.name):
+            continue
+        report_file = s3_obj.get()["Body"]  # type: botocore.response.StreamingBody
+        if report_path.suffix == ".lz4":
+            report_path = report_path.with_name(report_path.stem)
+            report_file = lz4.frame.open(report_file, "r")
+        with report_file:
+            if report_path.suffix == ".tar":
+                with tarfile.open(fileobj=report_file, mode="r|") as tar_file:
+                    for entry in tar_file:
+                        yield entry.name, tar_file.extractfile(entry)
+            else:
+                yield str(report_path), report_file
+
 
 def main(args):
-    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
-
+    LOGGER.setLevel(logging.DEBUG if args.debug else logging.INFO)
+    LOGGER.debug("Initializing Boto")
     # See https://ooni.torproject.org/post/mining-ooni-data/
-    s3 = boto3.resource("s3")  # type: boto3.session.Session.resource
+    s3 = boto3.resource("s3")  # type: boto3.resources.base.ServiceResource
     s3.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
     bucket = s3.Bucket("ooni-data")
-    for obj in itertools.islice(list_files(bucket, test_name=args.test_name, prefix=args.prefix), args.limit):
-        print("Fetching %s" % obj.key)
-        # TODO: Handle .tar.lz4 files
-        obj_body = obj.get()["Body"]  # type: botocore.response.StreamingBody
-        with lz4.frame.open(obj_body, "r") as obj_file:
-            if obj.key.endswith(".tar.lz4"):
-                with tarfile.open(fileobj=obj_file, mode="r|") as tar_file:
-                    for entry in tar_file:
-                        print(entry.name)
-                        print(tar_file.extractfile(entry).read(100))
-            else:
-                # TODO: trim measurements
-                print(obj_file.read(100))
+    for filename, fileobj in itertools.islice(read_report_files(
+            bucket, test_name=args.test_name, prefix=args.prefix), args.limit):
+        with fileobj:
+            # TODO: trim measurements
+            if LOGGER.isEnabledFor(logging.DEBUG):
+                LOGGER.debug("Reading %s" % filename)
+                print("  %s" % fileobj.read(80))
 
 
 if __name__ == "__main__":
