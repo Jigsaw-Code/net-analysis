8d66cb7e003c0bb3a4e3950bf6513b145b4e9500
Jon Cohen <cohenjon@google.com>
2020-02-21 23:07:17 +0000

    Respond to some review comments
    Also un-ignores the build and distribution directories for pip to grab
    straight form the github repo

--- setup.py
+++ setup.py
@@ -4,25 +4,24 @@ import setuptools
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
-deps = os.popen('pip freeze').readlines()
+deps = os.popen("pip freeze").readlines()
 # readlines() doesn't strip trailing newlines
 deps = [d.strip() for d in deps]
 
 setuptools.setup(
-    name="net-analysis",
+    name="jigsaw-net-analysis",
     version="0.0.1",
     author="Jigsaw Operations, LLC",
     author_email="fortuna@google.com",
-    description="Network censorship detection tools",
+    description="Network analysis tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Jigsaw-Code/net-analysis",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX :: Linux",
+        "Operating System :: OS Independent",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries",
         "Topic :: System :: Networking :: Monitoring"
