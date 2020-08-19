4e5a370f2554fc6c04889a352ffa533c99d9a53e
Jonathan Cohen <cohenjon@google.com>
2020-08-19 12:00:22 -0400

    Fix simple_autonomous_aystem test, add ip_info test

--- netanalysis/ip/simple_autonomous_system_test.py
+++ netanalysis/ip/simple_autonomous_system_test.py
@@ -19,27 +19,22 @@ import unittest
 from . import simple_autonomous_system as sas
 
 
-class NoneIpToAsnMap(sas.IpToAsnMap):
-    def get_asn(self, ip_address):
-        return None
-
-
 class TestAsRepository(unittest.TestCase):
     def test_unknown_as(self):
-        as_repo = sas.InMemoryAsRepository(NoneIpToAsnMap())
+        as_repo = sas.InMemoryAsRepository()
         asys = as_repo.get_as(999)
         self.assertIsNotNone(asys)
         self.assertEqual(999, asys.id)
         self.assertEqual("AS999", asys.name)
 
     def test_unknown_org(self):
-        as_repo = sas.InMemoryAsRepository(NoneIpToAsnMap)
+        as_repo = sas.InMemoryAsRepository()
         org = as_repo.get_org("orgX")
         self.assertIsNotNone(org)
         self.assertEqual("orgX", org.id)
 
     def test_add_as(self):
-        as_repo = sas.InMemoryAsRepository(NoneIpToAsnMap)
+        as_repo = sas.InMemoryAsRepository()
         as_repo.add_as("AS1", "First AS", "org1", "test_data", "sometime")
         as1 = as_repo.get_as("AS1")
         self.assertEqual("AS1", as1.id)
@@ -47,7 +42,7 @@ class TestAsRepository(unittest.TestCase):
         self.assertEqual("org1", as1.org.id)
 
     def test_add_org(self):
-        as_repo = sas.InMemoryAsRepository(NoneIpToAsnMap)
+        as_repo = sas.InMemoryAsRepository()
         as_repo.add_org("org1", "First Org", "Country1",
                         "test_data", "sometime")
         org = as_repo.get_org("org1")
@@ -56,7 +51,7 @@ class TestAsRepository(unittest.TestCase):
         self.assertEqual("Country1", org.country)
 
     def test_as_org(self):
-        as_repo = sas.InMemoryAsRepository(NoneIpToAsnMap)
+        as_repo = sas.InMemoryAsRepository()
         as_repo.add_as("AS1", "First AS", "org1", "test_data", "sometime")
         as_repo.add_org("org1", "First Org", "Country1",
                         "test_data", "sometime")
@@ -64,14 +59,6 @@ class TestAsRepository(unittest.TestCase):
         self.assertEqual("org1", org.id)
         self.assertEqual("First Org", org.name)
         self.assertEqual("Country1", org.country)
-
-
-class TestIpToAsnMap(unittest.TestCase):
-    def test_default_data(self):
-        ip_asn_map = sas.create_default_ip_asn_map()
-        self.assertEqual(15169, ip_asn_map.get_asn("8.8.8.8"))
-        self.assertEqual(15169, ip_asn_map.get_asn("8.8.4.4"))
-        self.assertEqual(-1, ip_asn_map.get_asn("127.0.0.1"))
 
 
 if __name__ == '__main__':
