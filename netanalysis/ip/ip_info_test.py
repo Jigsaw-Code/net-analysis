7b4160a781a36b9b30851f470870ca1dc0666c10
Jonathan Cohen <cohenjon@google.com>
2020-08-19 13:11:07 -0400

    Use a single ip_info service for speed

--- netanalysis/ip/ip_info_test.py
+++ netanalysis/ip/ip_info_test.py
@@ -6,41 +6,39 @@ from . import ip_info as ii
 
 
 class TestIpInfo(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    cls._ip_service = ii.create_default_ip_info_service()
+
   def test_ip4_to_as(self):
-    ip_service = ii.create_default_ip_info_service()
-    self.assertEqual(15169, ip_service.get_as(ip("8.8.8.8")).id)
-    self.assertEqual(15169, ip_service.get_as(ip("8.8.4.4")).id)
-    self.assertEqual(-1, ip_service.get_as(ip("127.0.0.1")).id)
+    self.assertEqual(15169, self._ip_service.get_as(ip("8.8.8.8")).id)
+    self.assertEqual(15169, self._ip_service.get_as(ip("8.8.4.4")).id)
+    self.assertEqual(-1, self._ip_service.get_as(ip("127.0.0.1")).id)
   
   def test_ip6_to_as(self):
-    ip_service = ii.create_default_ip_info_service()
-    self.assertEqual(15169, ip_service.get_as(ip("2001:4860:4860::8888")).id)
-    self.assertEqual(15169, ip_service.get_as(ip("2001:4860:4860::8844")).id)
-    self.assertEqual(-1, ip_service.get_as(ip("::1")).id)
+    self.assertEqual(15169, self._ip_service.get_as(ip("2001:4860:4860::8888")).id)
+    self.assertEqual(15169, self._ip_service.get_as(ip("2001:4860:4860::8844")).id)
+    self.assertEqual(-1, self._ip_service.get_as(ip("::1")).id)
 
   def test_ip4_to_country(self):
-    ip_service = ii.create_default_ip_info_service()
-    self.assertEqual(("US", "United States"), ip_service.get_country(ip("8.8.8.8")))
-    self.assertEqual(("US", "United States"), ip_service.get_country(ip("8.8.4.4")))
-    self.assertEqual(("ZZ", "Unknown"), ip_service.get_country(ip("127.0.0.1")))
+    self.assertEqual(("US", "United States"), self._ip_service.get_country(ip("8.8.8.8")))
+    self.assertEqual(("US", "United States"), self._ip_service.get_country(ip("8.8.4.4")))
+    self.assertEqual(("ZZ", "Unknown"), self._ip_service.get_country(ip("127.0.0.1")))
 
   def test_ip6_to_country(self):
-    ip_service = ii.create_default_ip_info_service()
-    self.assertEqual(("US", "United States"), ip_service.get_country(ip("2001:4860:4860::8888")))
-    self.assertEqual(("US", "United States"), ip_service.get_country(ip("2001:4860:4860::8844")))
-    self.assertEqual(("ZZ", "Unknown"), ip_service.get_country(ip("::1")))
+    self.assertEqual(("US", "United States"), self._ip_service.get_country(ip("2001:4860:4860::8888")))
+    self.assertEqual(("US", "United States"), self._ip_service.get_country(ip("2001:4860:4860::8844")))
+    self.assertEqual(("ZZ", "Unknown"), self._ip_service.get_country(ip("::1")))
   
   def test_resolve_ip4(self):
-    ip_service = ii.create_default_ip_info_service()
-    self.assertEqual("dns.google", ip_service.resolve_ip(ip("8.8.8.8")))
-    self.assertEqual("dns.google", ip_service.resolve_ip(ip("8.8.4.4")))
-    self.assertEqual("localhost", ip_service.resolve_ip(ip("127.0.0.1")))
+    self.assertEqual("dns.google", self._ip_service.resolve_ip(ip("8.8.8.8")))
+    self.assertEqual("dns.google", self._ip_service.resolve_ip(ip("8.8.4.4")))
+    self.assertEqual("localhost", self._ip_service.resolve_ip(ip("127.0.0.1")))
 
   def test_resolve_ip4(self):
-    ip_service = ii.create_default_ip_info_service()
-    self.assertEqual("dns.google", ip_service.resolve_ip(ip("2001:4860:4860::8888")))
-    self.assertEqual("dns.google", ip_service.resolve_ip(ip("2001:4860:4860::8844")))
-    self.assertEqual("localhost", ip_service.resolve_ip(ip("::1")))
+    self.assertEqual("dns.google", self._ip_service.resolve_ip(ip("2001:4860:4860::8888")))
+    self.assertEqual("dns.google", self._ip_service.resolve_ip(ip("2001:4860:4860::8844")))
+    self.assertEqual("localhost", self._ip_service.resolve_ip(ip("::1")))
 
 
 if __name__ == '__main__':
