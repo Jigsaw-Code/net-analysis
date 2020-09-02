a01073fa50d908a36cfabfc265aa5980ac2c41ed
Jonathan Cohen <cohenjon@google.com>
2020-09-02 12:27:41 -0400

    Use DB-IP's free IP to AS and IP to Country databases

--- netanalysis/ip/ip_info.py
+++ netanalysis/ip/ip_info.py
@@ -71,9 +71,9 @@ class IpInfoService:
 def create_default_ip_info_service() -> IpInfoService:
     as_repo = sas.create_default_as_repo()
     ip_asn = geoip2.database.Reader(resource_filename(
-        "third_party/maxmind/GeoLite2-ASN/GeoLite2-ASN.mmdb"))
+        "third_party/db-ip/dbip-asn/dbip-asn-lite.mmdb"))
     ip_country = geoip2.database.Reader(resource_filename(
-        "third_party/maxmind/GeoLite2-Country/GeoLite2-Country.mmdb"))
+        "third_party/db-ip/dbip-country/dbip-country-lite.mmdb"))
     return IpInfoService(as_repo, ip_asn, ip_country)
 
 
